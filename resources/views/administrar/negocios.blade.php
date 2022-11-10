ea39c9efbbf8cfd73c231c986c9cb9616ba56c89
Eduardo De La Cruz <eduardo352885@gmail.com>
2022-11-10 14:09:47 -0600

    vista de negocios

---
+++ resources/views/administrar/negocios.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.template')
+
+@section('content')
+<link rel="stylesheet">
+
+<div class="container">
+    <h4 class="text-white text-center mt-2 mb-2">Administrar negocios</h4>
+
+</div>
+@endsection
