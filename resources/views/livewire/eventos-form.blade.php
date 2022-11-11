4f2cfedda368db129e9532043e855ecf57132bff
Emanuel Alamilla <e.alamilla318@gmail.com>
2022-11-11 15:07:05 -0600

    new migrations and basic insertion of events complete

--- resources/views/livewire/eventos-form.blade.php
+++ resources/views/livewire/eventos-form.blade.php
@@ -28,11 +28,15 @@
     </div>
 
     <label style="color: black !important;">
-        <p class="m-0">Imagenes (Se necesitan 3 imagenes)</p>
+        <p class="m-0">Imagenes (10 imagenes maximo)</p>
     </label>
-    <div  class="input-group mb-3">
+    <div class="input-group mb-3">
         <input wire:model="images" multiple type="file" class="form-control">
     </div>
+    <div wire:loading wire:target="images" class="spinner-border text-warning" role="status">
+        <span class="visually-hidden">Loading...</span>
+    </div>
+
     <label style="color: black !important;">
         <p class="m-0">Localizacion del lugar</p>
     </label>
@@ -42,11 +46,36 @@
     </div>
 
 
-    <input type="text" wire:model.defer="latitude" hidden id="latitud" value="">
-    <input type="text" wire:model.defer="longitude" hidden id="longitud" value="">
+    <input type="text" wire:model="latitude" hidden id="latitud" value="">
+    <input type="text" wire:model="longitude" hidden id="longitud" value="">
 
-
+    @error('name')
+        <div class="alert alert-danger" role="alert">
+            <strong>{{ $message }}</strong>
+        </div>
+    @enderror
+    @error('description')
+        <div class="alert alert-danger" role="alert">
+            <strong>{{ $message }}</strong>
+        </div>
+    @enderror
+    @error('dateStart')
+        <div class="alert alert-danger" role="alert">
+            <strong>{{ $message }}</strong>
+        </div>
+    @enderror
+    @error('dateEnd')
+        <div class="alert alert-danger" role="alert">
+            <strong>{{ $message }}</strong>
+        </div>
+    @enderror
+    @error('images')
+        <div class="alert alert-danger" role="alert">
+            <strong>{{ $message }}</strong>
+        </div>
+    @enderror
     <div class="text-center">
-        <button type="button" wire:click="createEvento" class="btn btn-round bg-gradient-info btn-lg w-100 mt-4 mb-0">Aceptar</button>
+        <button type="button" wire:click="createEvento"
+            class="btn btn-round bg-gradient-info btn-lg w-100 mt-4 mb-0">Aceptar</button>
     </div>
 </div>
